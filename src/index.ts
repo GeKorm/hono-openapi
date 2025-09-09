b078c3294d80780eae422a00587593dc5066de06
GeKorm <mail@gekorm.com>
2025-09-09 13:02:14 +0100

    Reduce size

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-export { generateSpecs, openAPIRouteHandler } from "./handler.js";
-export {
-  describeResponse,
-  describeRoute,
-  resolver,
-  validator,
-} from "./middlewares.js";
-export type * from "./types.js";
-export * from "./utils.js";
