5ca2fca876f11e509056049f99620f605e1ae37e
GeKorm <mail@gekorm.com>
2025-09-27 22:43:41 +0100

    fix: compatibility with zod v4 and zod-openapi v5
    - Update tests to reflect newest zod v4
    - Add legacy overrides script to support zod v3 + zod-openapi v4 as well as zod v4 + zod-openapi v5
    - Fix `components` schema merging

--- src/utils.ts
+++ src/utils.ts
@@ -302,3 +302,7 @@ export function removeExcludedPaths(
 
   return newPaths;
 }
+
+export function getComponents(components: OpenAPIV3_1.ComponentsObject) {
+  return components.schemas ? components : { schemas: components };
+}
